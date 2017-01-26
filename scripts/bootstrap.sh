4037d14ccb0db6b28c5b8ff62dc92828376a470e
http://airccse.org/journal/jcsit/0611csit01.pdf <zorrion2000@yahoo.com>
2017-01-25 22:48:15 -0600

    Added additional download
    Added additional download for vtadrones/geoshape

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,14 @@
 set -e
 cd /etc/yum.repos.d/
 wget  https://yum.boundlessps.com/geoshape.repo
+mkdir -p /var/lib/geonode/rogue_geonode/geoshape
+cd /var/lib/geonode/rogue_geonode/
+sudo wget https://github.com/vtadrones/geoshape-vagrant/archive/master.zip
+sudo yum -y install unzip
+sudo unzip master.zip
+sudo cp -r geoshape-vagrant-master/ geoshape
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 rpm -Uvh epel-release-latest-6.noarch.rpm
 rpm -Uvh http://yum.postgresql.org/9.6/redhat/rhel-6-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 yum -y install geoshape geoshape-geoserver elasticsearch postgis-postgresql95 rabbitmq-server-3.6.1
+
